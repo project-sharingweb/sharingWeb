07217e0311d8ab012f6c2e0f4cdbe53e32c6243b
Guille LA <guillermolucena90@gmail.com>
2019-06-23 17:31:45 +0200

    error handling added, added secure middleware, integrates secure mid to some routes

--- sharingWeb-api/routes/shop.routes.js
+++ sharingWeb-api/routes/shop.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const shopController = require('../controllers/shop.controller')
+const secure = require('../middlewares/secure.mid')
 
 router.get('/', homeController.shops)
 
@@ -8,9 +9,9 @@ router.get('/:name', shopController.home)
 
 router.get('/:name/products', shopController.products)
 router.get('/:name/products/:id', shopController.productDetail)
-router.post('/addproduct', shopController.addProduct)
+router.post('/addproduct', secure.isAuthenticated, shopController.addProduct)
 
-router.get('/:name/orders', shopController.orders)
+router.get('/:name/orders', secure.isAuthenticated, shopController.orders)
 router.post('/:name/purchase', shopController.purchase)
 
 
