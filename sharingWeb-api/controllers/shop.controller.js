bafcdba9569fd634b550a9480f76a9117ef51171
Guille LA <guillermolucena90@gmail.com>
2019-06-24 18:49:07 +0200

    corrected some bugs

--- sharingWeb-api/controllers/shop.controller.js
+++ sharingWeb-api/controllers/shop.controller.js
@@ -1,3 +1,21 @@
 const Shop = require('../models/shop.model')
 const Product = require('../models/product.model')
 const Order = require('../models/order.model')
+
+
+module.exports.shops = (req, res, next) => {}
+
+
+module.exports.home = (req, res, next) => {}
+
+
+module.exports.products = (req, res, next) => {}
+
+module.exports.productDetail = (req, res, next) => {}
+
+module.exports.addProduct = (req, res, next) => {}
+
+module.exports.orders = (req, res, next) => {}
+
+module.exports.purchase = (req, res, next) => {}
+
