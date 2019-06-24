271fcb2d155d0d8b8875ad43778535afbf55557a
MartaVaqgut <martavaquerogutierrez@gmail.com>
2019-06-24 19:29:27 +0200

    top model

--- sharingWeb-api/routes/shop.routes.js
+++ sharingWeb-api/routes/shop.routes.js
@@ -3,17 +3,16 @@ const router = express.Router();
 const shopController = require('../controllers/shop.controller')
 const secure = require('../middlewares/secure.mid')
 
-router.get('/', shopController.shops)
+router.get('/', shopController.list)
+router.get('/:name', shopController.shopDetail)
 
-router.get('/:name', shopController.home)
-
-router.get('/:name/products', shopController.products)
+router.get('/:name/products', shopController.productsList)
 router.get('/:name/products/:id', shopController.productDetail)
 router.post('/addproduct', secure.isAuthenticated, shopController.addProduct)
 
-router.get('/:name/orders', secure.isAuthenticated, shopController.orders)
+router.get('/:name/orders', secure.isAuthenticated, shopController.ordersList)
+router.get('/:name/orders/:id', secure.isAuthenticated, shopController.ordersDetail)
 router.post('/:name/purchase', shopController.purchase)
-
 
 
 module.exports = router;
