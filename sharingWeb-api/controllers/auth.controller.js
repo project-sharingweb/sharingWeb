0b75430b761a4842ef96f2398a3fca5c3c75ca95
Guille LA <guillermolucena90@gmail.com>
2019-06-27 17:19:22 +0200

    added gitignore correctly

--- sharingWeb-api/controllers/auth.controller.js
+++ sharingWeb-api/controllers/auth.controller.js
@@ -19,6 +19,24 @@ module.exports.register = (req, res, next) => {
     .catch(next)
 }
 
+module.exports.editShop = (req, res, next) => {
+  const { email } = req.user
+  console.log(email)
+
+  Shop.findOne({email: email})
+    .then(shop => {
+      if (shop) {
+        for(let k in req.body) shop[k]=req.body[k]
+        return shop.save()
+      }
+      else {
+        createError(404, 'shop not found')
+      }
+    })
+    .then(shop => res.status(201).json(shop))
+    .catch(next)
+}
+
 
 
 module.exports.login = (req, res, next) => {
--- sharingWeb-api/routes/auth.routes.js
+++ sharingWeb-api/routes/auth.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller')
+const secure = require('../middlewares/secure.mid')
 
 
 /* GET home page. */
 router.post('/register', authController.register)
+router.put('/editshop', secure.isAuthenticated, authController.editShop)
 
 router.post('/login', authController.login)
 
