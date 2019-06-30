664cda405e0cba704a087fccc773049d0f238bd3
MartaVaqgut <martavaquerogutierrez@gmail.com>
2019-06-30 11:20:43 +0200

    top model api

--- sharingWeb-api/models/shop.model.js
+++ sharingWeb-api/models/shop.model.js
@@ -18,56 +18,60 @@ const modifyName = (name) => {
 }
 
 const shopSchema = new mongoose.Schema({
-   email: {
-       type: String,
-       match: [EMAIL_PATTERN, 'Invalid name pattern'],
-       required: true,
-       unique: true,
-       trim: true,
-       lowercase: true
-   },
-   password: {
-       type: String,
-       required: true,
-       minlength: 8,
-       match: [PASS_PATTERN, 'Invalid name pattern']
-   },
-   name: {
-       type: String,
-       required: true,
-       unique: true,
-       lowercase: true
-   },
-   logo: {
-       type: String,
-       match: [URL_PATTERN, 'Invalid URl pattern']
-   },
-   bgImage: {
-       type: String,
-       match: [URL_PATTERN, 'Invalid URl pattern'],
-       default: "https://outven.net/wp-content/uploads/2018/09/outven-background-landing-usa.jpg"
-   },
-   navbarColor: {
-       type: String,
-       default: "white"
-   },
-   aboutUs: {
-       type: String,
-   },
-   moto: {
-       type: String,
-       maxlength: 144,
-   },
-   contact: {
-       type: String,
-   },
-   locations: {
-       type: {
-           type: String
-       },
-       coordinates: [Number]
-   }
-   }, {timestamps: true, toJSON:{
+    email: {
+        type: String,
+        match: [EMAIL_PATTERN, 'Invalid name pattern'],
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        match: [PASS_PATTERN, 'Invalid name pattern']
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    aboutUs: {
+        type: String,
+    },
+    moto: {
+        type: String,
+        maxlength: 144,
+    },
+    contact: {
+        type: String,
+    },
+    locations: {
+        type: {
+            type: String
+        },
+        coordinates: [Number]
+    },
+    styles: {
+        logo: {
+            type: String,
+            match: [URL_PATTERN, 'Invalid URl pattern']
+        },
+        bgImage: {
+            type: String,
+            match: [URL_PATTERN, 'Invalid URl pattern'],
+            default: "https://outven.net/wp-content/uploads/2018/09/outven-background-landing-usa.jpg"
+        },
+        nav: {
+            type: String,
+            default: {
+                backgroundcolor: "White",
+            } 
+        }
+    }
+    }, {timestamps: true, toJSON:{
        virtuals: true,
        transform: (doc, ret ) => {
            ret.id = ret._id;
@@ -76,26 +80,27 @@ const shopSchema = new mongoose.Schema({
            delete ret.__v
            return ret;
        }
-   }
+    }
+
 })
 
 shopSchema.pre('save', function(next){
-  const shop = this
-  shop.name = modifyName(shop.name)
-  if(shop.isModified("password")){
-      bcrypt.genSalt(SALT_FACTOR)
-          .then (salt => {
-              return bcrypt.hash(shop.password, salt)
-                  .then(hash => {
-                      shop.password=hash
-                      next()
-                  })
-          })
-          .catch(next)
-  }
-  else{
-      next()
-  }
+    const shop = this
+    shop.name = modifyName(shop.name)
+    if(shop.isModified("password")){
+        bcrypt.genSalt(SALT_FACTOR)
+            .then (salt => {
+                return bcrypt.hash(shop.password, salt)
+                    .then(hash => {
+                        shop.password=hash
+                        next()
+                    })
+            })
+            .catch(next)
+    }
+    else{
+        next()
+    }
 })
 
 shopSchema.methods.checkPassword = function(password){
