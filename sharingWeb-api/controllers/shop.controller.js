b26fa4527f8ce6099ba8d0cbc7b8d49a58173946
Guille LA <guillermolucena90@gmail.com>
2019-07-11 01:09:49 +0200

    added paypal and nodemailer, added deletion of products

--- sharingWeb-api/controllers/shop.controller.js
+++ sharingWeb-api/controllers/shop.controller.js
@@ -2,6 +2,9 @@ const Shop = require('../models/shop.model')
 const Product = require('../models/product.model')
 const Order = require('../models/order.model')
 const paypal = require('paypal-rest-sdk')
+const nodemailer = require('nodemailer');
+const user = process.env.MAIL_USER
+const pass = process.env.MAIL_PASS
 
 
 module.exports.list = (req, res, next) => {
@@ -48,6 +51,14 @@ module.exports.addProduct = (req, res, next) => {
     .catch(next)
 }
 
+module.exports.deleteProduct = (req, res, next) => {
+  const product = req.body
+  console.log(product.id)
+
+  Product.findByIdAndDelete(product.id)
+    .then(product => res.status(200))
+    .catch(next)
+} 
 
 module.exports.ordersList = (req, res, next) => {
   const shopName = req.params.name
@@ -69,7 +80,6 @@ module.exports.ordersDetail = (req, res, next) => {
 
 
 module.exports.purchase = async (req, res, next) => {
-  console.log(req.body)
   const order = new Order(req.body)
 
   await order.save()
@@ -80,24 +90,27 @@ module.exports.purchase = async (req, res, next) => {
         payment_method: "paypal"
     },
     redirect_urls: {
-        return_url: "http://return.url",
-        cancel_url: "http://cancel.url"
+        return_url: `http://localhost:3001/shops/${req.body.shopName}/orders/${order.id}/modify`,
+        cancel_url: `http://localhost:3000/shops/${req.body.shopName}/failed`
     },
     transactions: [{
         item_list: {
-            items: [{
-                name: "item",
-                sku: "item",
-                price: "1.00",
+            items: req.body.cart.map(item => {
+              const obj = {
+                name: item.name,
+                sku: item.id,
+                price: parseInt(item.price),
                 currency: "EUR",
-                quantity: 1
-            }]
+                quantity: item.amount
+              }
+              return obj
+            })
         },
         amount: {
             currency: "EUR",
-            total: "1.00"
+            total: req.body.cart.reduce((acc, item)=> acc + (parseInt(item.price)*item.amount), 0).toString()
         },
-        description: "Articles at Tienda Modelo"
+        description: `Order for articles purchased at ${req.body.shopName}`
     }]
   };
   
@@ -110,7 +123,43 @@ module.exports.purchase = async (req, res, next) => {
         res.json({ url: payment.links[1].href })
     }
   });
+
+  
 }
+
+module.exports.confirmPayment = (req, res, next) => {
+  const id = req.params.id
+
+  Order.findById(id)
+    .then(order => {
+      if (order) {
+        order.status = "in process"
+        return order.save().then( order => {
+          
+          let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: { user, pass }
+          });
+          
+          transporter.sendMail({
+            from: `"My Awesome Project 👻"`,
+            to: "guillermolucena@hotmail.com",
+            subject: 'Hola Julio!!!!', 
+            text: 'Awesome Message',
+            html: '<b style="width: 500px; height: 500px; color: blue; background-color: red; border: green solid 3px; font-size: 5rem;">Funciona!</b>'
+          })
+            .then(info => console.log(info))
+            .catch(error => console.log(error))
+
+          res.status(200).redirect(`http://localhost:3000/shops/${order.shopName}/success/${order.id}`)
+        })
+      }
+      else createError(404, 'shop not found')
+    })
+    .catch(next)
+
+}
+
 
 module.exports.editShop = (req, res, next) => {
   const shop = req.body
